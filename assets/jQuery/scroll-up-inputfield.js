adf39e00ec1f0e8ba7ec69b3bcf10351e08365fc
hradiluhung <hakamra107@gmail.com>
2022-02-07 06:54:32 +0700

    scrollup textfiled

--- assets/jQuery/scroll-up-inputfield.js
+++ assets/jQuery/scroll-up-inputfield.js
@@ -1,9 +1,11 @@
-var $htmlOrBody = $('html, body'), // scrollTop works on <body> for some browsers, <html> for others
+if ($(window).width() <= 600) {
+  var $htmlOrBody = $('html, body'), // scrollTop works on <body> for some browsers, <html> for others
     scrollTopPadding = 8;
 
-$('textarea').focus(function() {
+  $('textarea').focus(function () {
     // get textarea's offset top position
     var textareaTop = $(this).offset().top;
     // scroll to the textarea
     $htmlOrBody.scrollTop(textareaTop - scrollTopPadding);
-});
+  });
+}
