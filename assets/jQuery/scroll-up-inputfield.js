c1903451330179a7d5138f55a27d0029c2407b10
hradiluhung <hakamra107@gmail.com>
2022-02-07 07:02:58 +0700

    langsung aja

--- assets/jQuery/scroll-up-inputfield.js
+++ assets/jQuery/scroll-up-inputfield.js
@@ -1,11 +1,12 @@
 if ($(window).width() <= 600) {
-  var $htmlOrBody = $('html, body'), // scrollTop works on <body> for some browsers, <html> for others
+  var $modal = $('#tambah-soal-modal, #edit-soal-modal'), // scrollTop works on <body> for some browsers, <html> for others
     scrollTopPadding = 8;
 
   $('textarea').focus(function () {
     // get textarea's offset top position
     var textareaTop = $(this).offset().top;
     // scroll to the textarea
-    $htmlOrBody.scrollTop(textareaTop - scrollTopPadding);
+    $modal.css('top', '20px');
+    $modal.scrollTop(textareaTop - scrollTopPadding);
   });
 }
